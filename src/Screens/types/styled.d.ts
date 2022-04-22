df08fb94f861b6dc746c031905019f8e733a7e27
santosfk <santosguilhermefk@gmail.com>
2022-04-22 15:41:50 -0300

    feat : adding theme typing

---
+++ src/Screens/types/styled.d.ts
@@ -0,0 +1,12 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    COLOR: {
+      BACKGROUND: string;
+      TEXT: string;
+      GREEN: string;
+      RED: string;
+    };
+  }
+}
