cf08d5f81ea09e785ad9d846e21fe802b729b1a9
santosfk <santosguilhermefk@gmail.com>
2022-04-23 11:17:03 -0300

    feat: adding responsiveness to signup screen

--- src/Screens/Signup/style.ts
+++ src/Screens/Signup/style.ts
@@ -4,11 +4,34 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+
+  @media (min-width: 320px) {
+    button {
+      font-size: 1rem;
+    }
+  }
+  @media (min-width: 2560px) {
+    button {
+      font-size: 2rem;
+    }
+  }
 `;
 export const Title = styled.h1`
   font-size: 3rem;
   color: #0781f5;
   letter-spacing: 2px;
+  @media (min-width: 320px) {
+    font-size: 1.5rem;
+  }
+  @media (min-width: 768px) {
+    font-size: 2.2rem;
+  }
+  @media (min-width: 1440px) {
+    font-size: 3rem;
+  }
+  @media (min-width: 2560px) {
+    font-size: 4rem;
+  }
 `;
 export const RightContent = styled.div`
   width: 50%;
@@ -17,6 +40,16 @@ export const RightContent = styled.div`
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  @media (min-width: 320px) {
+    margin: auto;
+  }
+  @media (min-width: 320px) {
+    width: 100%;
+    height: 100vh;
+  }
+  @media (min-width: 1024px) {
+    width: 50%;
+  }
 `;
 export const EmailContent = styled.div`
   width: 100%;
@@ -28,6 +61,37 @@ export const EmailContent = styled.div`
   }
   span {
     color: red;
+  }
+  @media (min-width: 320px) {
+    margin: auto;
+    margin-bottom: 30px;
+    width: 250px;
+    input {
+      height: 20px;
+    }
+  }
+  @media (min-width: 375px) {
+    width: 300px;
+  }
+  @media (min-width: 425px) {
+    width: 350px;
+  }
+  @media (min-width: 768px) {
+    width: 400px;
+  }
+  @media (min-width: 1440px) {
+    width: 500px;
+    input {
+      height: 30px;
+    }
+  }
+  @media (min-width: 2560px) {
+    width: 800px;
+    margin: 50px 0px;
+    input {
+      height: 50px;
+      font-size: 2rem;
+    }
   }
 `;
 export const PasswordContent = styled.div`
@@ -41,6 +105,37 @@ export const PasswordContent = styled.div`
   span {
     color: red;
   }
+  @media (min-width: 320px) {
+    margin: auto;
+    margin-bottom: 30px;
+    width: 250px;
+    input {
+      height: 20px;
+    }
+  }
+  @media (min-width: 375px) {
+    width: 300px;
+  }
+  @media (min-width: 425px) {
+    width: 350px;
+  }
+  @media (min-width: 768px) {
+    width: 400px;
+  }
+  @media (min-width: 1440px) {
+    width: 500px;
+    input {
+      height: 30px;
+    }
+  }
+  @media (min-width: 2560px) {
+    width: 800px;
+    margin: 50px 0px;
+    input {
+      height: 50px;
+      font-size: 2rem;
+    }
+  }
 `;
 export const SubmitContent = styled.div`
   width: 100%;
@@ -49,12 +144,40 @@ export const SubmitContent = styled.div`
   button {
     width: 300px;
   }
+  @media (min-width: 320px) {
+    button {
+      width: 200px;
+    }
+  }
+  @media (min-width: 425px) {
+    button {
+      width: 250px;
+    }
+  }
+  @media (min-width: 1440px) {
+    button {
+      width: 300px;
+      height: 50px;
+    }
+  }
+  @media (min-width: 2560px) {
+    width: 800px;
+    button {
+      height: 70px;
+      font-size: 2rem;
+    }
+  }
 `;
 export const LeftContent = styled.div`
-  background-color: #31e089;
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  @media (min-width: 320px) {
+    display: none;
+  }
+  @media (min-width: 1024px) {
+    background-color: #31e089;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
 `;
