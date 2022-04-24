04e76c066553e0cfd9304ff309281d4913a7e5c7
santosfk <santosguilhermefk@gmail.com>
2022-04-24 20:04:47 -0300

    feat : typing application themes

---
+++ src/types/styled.d.ts
@@ -0,0 +1,12 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    COLOR: {
+      BACKGROUND: String;
+      TEXT: String;
+      GREEN: String;
+      RED: String;
+    };
+  }
+}
